95619f098dfc36e72d7ac1df310119fd88d62818
Anton1273751 <kronoss161@gmail.com>
2024-01-14 19:07:11 +0300

    work

--- app/index.js
+++ app/index.js
@@ -1,3 +1,37 @@
 import { news } from "./autors.js";
 
-console.log(news);
+const containerNode = document.querySelector(".autors__wrap");
+
+const createAutor = ({
+  author,
+  title,
+  description,
+  url,
+  source,
+  image,
+  category,
+  language,
+  country,
+  published_at,
+}) => {
+  const articleNode = document.createElement("article");
+  articleNode.className = "autor";
+  articleNode.innerHTML = ` <h3 class="autor__name">${author}</h3>
+            <title class="autor__title">${title}</title>
+            <p class="autor__description">${description}</p>
+            <a class="autor__url" href="">${url}</a>
+            <p class="autor__sourse">${source}</p>
+            <img class="autor__img" src="${image}" alt="#" />
+            <p class="autor__language">${category}</p>
+            <p class="autor__language">${language}</p>
+            <p class="autor__country">${country}</p>
+            <time class="autor__published_at">${published_at}</time>`;
+  return articleNode;
+};
+
+const renderCard = (news) => {
+  containerNode.innerHTML = "";
+  news.forEach((element) => containerNode.append(createAutor(element)));
+};
+
+renderCard(news);
