6a1f31f30d9c6a989e4f72849ea57b668fa48c5c
Anton1273751 <kronoss161@gmail.com>
2024-01-18 13:45:06 +0300

    test

--- src/app/index.js
+++ src/app/index.js
@@ -2,24 +2,6 @@ import { news } from "./autors.js";
 
 const autorsNode = document.querySelector(".autors__wrap");
 
-  // let imgLike = document.querySelector("autor__btn");
-  // let pNode = document.querySelector("like");
-  // imgLike.addEventListener("click", addClick);
-  // let count = 0;
-
-  // const addClick = () => {
-  //   count++;
-  //   pNode.innerHTML = `${count}`;
-  // };
-
-// imgLike.addEventListener("click", addClick);
-// let count = 0;
-
-// const addClick = () => {
-//   count++;
-//   pNode.innerHTML = `${count}`;
-// };
-
 const createAutor = ({
   author,
   title,
@@ -31,8 +13,15 @@ const createAutor = ({
   const articleNode = document.createElement("article");
   articleNode.className = "autor";
   let publicDate = new Date(Date.parse(published_at));
-
+  // let imgLike = document.querySelector(".autor__like");
+  let pNode = document.querySelector(".like");
+  // imgLike.addEventListener("click", addClick);
   let count = 0;
+
+  // const addClick = () => {
+  //   count++;
+  //   pNode.innerHTML = `${count}`;
+  // };
   articleNode.innerHTML = ` <strong class="autor__name">${author}</strong>
             <title class="autor__title">${title}</title>
             <p class="autor__description">${description}</p>
@@ -40,7 +29,7 @@ const createAutor = ({
             <img class="autor__img" src="${image}" alt="#" />
             <time class="autor__published_at">${publicDate}</time>
             <div class = "autor__article-evaluation" >
-            <img class= "autor__like" src = "../src/img/like.jpg">
+            <input class = "autor__like" type="image" value = '0'src="../src/img/like.jpg" data-counter/>
             <p class = "like">${count}</p>  
             </div>  `;
   return articleNode;
@@ -49,11 +38,10 @@ const createAutor = ({
 const renderCard = (news) => {
   autorsNode.innerHTML = "";
   news.forEach((element) => autorsNode.append(createAutor(element)));
-
 };
 
 // document.addEventListener("click", function (el) {
-//   if (el.target.dataset != undefined) {
+//   if (el.target.dataset.counter != undefined) {
 //     el.target.value++;
 //   }
 // });
