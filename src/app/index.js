395fa4c46d6a1dfaaa67034185564920689e956d
Anton1273751 <kronoss161@gmail.com>
2024-01-18 14:32:23 +0300

    test

--- src/app/index.js
+++ src/app/index.js
@@ -13,15 +13,6 @@ const createAutor = ({
   const articleNode = document.createElement("article");
   articleNode.className = "autor";
   let publicDate = new Date(Date.parse(published_at));
-  // let imgLike = document.querySelector(".autor__like");
-  let pNode = document.querySelector(".like");
-  // imgLike.addEventListener("click", addClick);
-  let count = 0;
-
-  // const addClick = () => {
-  //   count++;
-  //   pNode.innerHTML = `${count}`;
-  // };
   articleNode.innerHTML = ` <strong class="autor__name">${author}</strong>
             <title class="autor__title">${title}</title>
             <p class="autor__description">${description}</p>
@@ -29,8 +20,8 @@ const createAutor = ({
             <img class="autor__img" src="${image}" alt="#" />
             <time class="autor__published_at">${publicDate}</time>
             <div class = "autor__article-evaluation" >
-            <input class = "autor__like" type="image" value = '0'src="../src/img/like.jpg" data-counter/>
-            <p class = "like">${count}</p>  
+             <button class="autor__like-button">👍</button>
+             <span class="like-count">0</span> 
             </div>  `;
   return articleNode;
 };
@@ -38,6 +29,14 @@ const createAutor = ({
 const renderCard = (news) => {
   autorsNode.innerHTML = "";
   news.forEach((element) => autorsNode.append(createAutor(element)));
+  const likeButton = document.querySelector(".autor__like-button");
+  const likeCount = document.querySelector(".like-count");
+  likeButton.addEventListener("click", like);
+  let count = 0;
+  function like() {
+    count++;
+    likeCount.innerHTML = `${count}`;
+  }
 };
 
 // document.addEventListener("click", function (el) {
